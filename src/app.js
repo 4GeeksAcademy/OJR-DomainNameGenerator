e101a9336cbdb506665744390b59bc8f92e9d110
Oneil-R <167453370+Oneil-R@users.noreply.github.com>
2024-06-06 00:59:00 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adjectives = ["wonderful", "small"];
+  let noun = ["biker", "trucker"];
+  let extensions = [".geeks", ".uk", ".mia", ".sky", ".yolo"];
+  const bios = [];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let secondIndex = 0; secondIndex < adjectives.length; secondIndex++) {
+      for (let thirdIndex = 0; thirdIndex < noun.length; thirdIndex++) {
+        for (let fourthIndex = 0; fourthIndex < noun.length; fourthIndex++) {
+          bios.push(
+            `${pronoun[index]}${adjectives[secondIndex]}${noun[thirdIndex]}${extensions[fourthIndex]}`
+          );
+        }
+      }
+    }
+  }
+  console.log("List of Domain Names:");
+  for (const bio of bios) {
+    console.log(bio);
+  }
 };
